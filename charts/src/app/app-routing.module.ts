80b4d531e1587fbae234c4d64f68d928b336096e
Javier Fernandez <javier.fernandez@s-dos.es>
2020-08-19 12:13:59 +0200

    Added schematics, routing and line-chart example

--- charts/src/app/app-routing.module.ts
+++ charts/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { LineChartComponent } from './components/line-chart/line-chart.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'line-chart', component: LineChartComponent },
+  { path: '**', redirectTo: 'line-chart' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- charts/src/app/app.module.ts
+++ charts/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { ChartsModule } from 'ng2-charts';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LineChartComponent } from './components/line-chart/line-chart.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent],
+  declarations: [AppComponent, NavbarComponent, LineChartComponent],
   imports: [BrowserModule, AppRoutingModule, ChartsModule],
   providers: [],
   bootstrap: [AppComponent],
